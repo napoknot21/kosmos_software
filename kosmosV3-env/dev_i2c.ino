6ad109fcf6877fae68ac6b77ddc08573bc6f8717
KonkArLab <107851441+KonkArLab@users.noreply.github.com>
2024-07-01 12:59:33 +0200

    Update dev_i2c.ino : organisation de la transmission de données

--- kosmosV3-env/dev_i2c.ino
+++ kosmosV3-env/dev_i2c.ino
@@ -1,9 +1,16 @@
-/* 
- * Arduino code to send and receive I2C data
- */
+
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x04       // I2C address for Arduino
-int i2cData = 0;                 // the I2C data received
+
+int Data = 1;
+int Data_indent = 0;
+int i2cData[5] = {};                 // the I2C data received
+int step_mode = 1;
+int number_of_revolutions = 1;
+int max_speed = 0;
+int max_acceleration = 0;
+int pause_time = 5000;
+
 void setup(){
   Wire.begin(SLAVE_ADDRESS);
   Wire.onReceive(receiveData);
@@ -11,17 +18,26 @@ void setup(){
   Serial.begin(9600);
 }
 void loop() {
-  // Everything happens in the interrupts
+  for (int j = 0; j<=4; j++){
+    Serial.print(i2cData[j]);
+    Serial.print(" ; ");
+    }
+  Serial.println("");
+  delay(5000);
 }
 // Handle reception of incoming I2C data
 void receiveData(int byteCount) {
   while (Wire.available()) {
-    i2cData = Wire.read();
-    Serial.println(i2cData);
+    Data = Wire.read();
+    if (!Data) {Data_indent = 0;}
+    else{
+      i2cData[Data_indent] = Data;
+      ++Data_indent;
+    }
   }
 }
 
 // Handle request to send I2C data
-void sendData() { 
-  Wire.write(i2cData);
+void sendData() {
+  Wire.write(1);
 }
