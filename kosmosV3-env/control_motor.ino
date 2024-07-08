b1cba66752d68081cfa0b6741ddad3257feeaaf6
KonkArLab <107851441+KonkArLab@users.noreply.github.com>
2024-07-08 16:42:25 +0200

    Update control_motor.ino : sleep mode fonctionnel
    Détection de l'interruption awake from raspberry sans risque de réveil non désiré dû au bruit dans les gpio au shutdown ou allumage du raspi
    Suppression du paramètre pause_time dans la communication i2c (plus utile)

--- kosmosV3-env/control_motor.ino
+++ kosmosV3-env/control_motor.ino
@@ -1,27 +1,31 @@
 #include "AccelStepper.h"
+#include "avr/sleep.h"
 #include <Wire.h>
-#define SLAVE_ADDRESS 0x04       // I2C address for Arduino
+#define SLAVE_ADDRESS 0x04  // I2C address for Arduino
 
 // Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver:
 #define dirPin 4
 #define stepPin 5
 #define SleepModePin 6
+#define wakeUpPin 3
 
-// paramètres d'interruption
+// paramètres d'interruption ils
 #define interrupt_pin 2
 #define motorInterfaceType 1
 volatile unsigned long button_time = 0;
 volatile unsigned long last_button_time = 0;
-int debounce = 2000;
+int debounce = 1000;
 
 //état de l'interrupteur ILS
 bool state_auto = 0;
 //état commandé par la Raspberry via i2c
 bool state_i2c = 0;
-//état de la connexion i2c 
+//état de la connexion i2c
 bool i2c_detected = false;
 //indicateur de fin de rotation moteur
 bool rotation_done = true;
+//indicateur pour se mettre en deep sleep
+bool goToSleep = 0;
 
 // structures pour les infos i2c
 int Data = 1;
@@ -33,19 +37,19 @@ int number_of_revolutions = 10,
     max_speed = 150,
     max_acceleration = 150,
     pause_time = 5,
-    step_mode = 4; // 1 pour full_step, 2 pour 1/2 microstep, 4 pour 1/4 microstep, 16 pour 1/16 microstep etc
-
+    step_mode = 4;  // 1 pour full_step, 2 pour 1/2 microstep, 4 pour 1/4 microstep, 16 pour 1/16 microstep etc
 
 // Création d'une instance de classe AccelStepper:
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
+  pinMode(wakeUpPin, INPUT);
   pinMode(SleepModePin, OUTPUT);
-  digitalWrite(SleepModePin, LOW); //au démarrage, on met le contrôleur moteur en mode sleep
+  digitalWrite(SleepModePin, LOW);  //au démarrage, on met le contrôleur moteur en mode sleep
 
   // Set the maximum speed and acceleration:
-  stepper.setMaxSpeed(max_speed*step_mode*10);
-  stepper.setAcceleration(max_acceleration*step_mode*10);
+  stepper.setMaxSpeed(max_speed * step_mode * 10);
+  stepper.setAcceleration(max_acceleration * step_mode * 10);
   stepper.disableOutputs();
 
   // définition de l'interruption liée à l'ILS pour déclencher le fonctionnement moteur indépendant
@@ -64,18 +68,35 @@ void loop() {
   if (!i2c_detected & state_auto) {
     motorRotate();
     // Attente entre 2 rotations
-    delay(pause_time*1000);
+    delay(pause_time * 1000);
   }
-  if (i2c_detected & state_i2c & !rotation_done) {
-    motorRotate();
-    rotation_done = true;
+
+  if (i2c_detected) {
+    if (state_i2c & !rotation_done) {
+      motorRotate();
+      rotation_done = true;
+    }
+    if (goToSleep) {
+      goToSleep = 0;
+
+      bool isAwake = 0;
+      while (!isAwake) {
+        attachInterrupt(digitalPinToInterrupt(wakeUpPin), wakeUp, HIGH);
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_enable();
+        sleep_cpu();
+        detachInterrupt(digitalPinToInterrupt(wakeUpPin));
+        delay(100);
+        isAwake = digitalRead(wakeUpPin);
+      }
+    }
   }
 }
 
+void wakeUp() {}
+
 // fonction appelée par l’interruption externe n°0
-void change_state()
-{
-  Serial.println("front détecté");
+void change_state() {
   button_time = millis();
   if (button_time > last_button_time + debounce) {
     state_auto = !state_auto;  // inverse l’état de la variable
@@ -83,13 +104,17 @@ void change_state()
   }
 }
 
-// fonction appelée à la réception d'un octet i2c 
+// fonction appelée à la réception d'un octet i2c
 void receiveData(int byteCount) {
   while (Wire.available()) {
-    if (!i2c_detected) {i2c_detected = true;}
+    if (!i2c_detected) {
+      i2c_detected = true;
+      detachInterrupt(digitalPinToInterrupt(interrupt_pin));
+    }
     Data = Wire.read();
-    if (!Data) {Data_indent = 0;}
-    else{
+    if (!Data) {
+      Data_indent = 0;
+    } else {
       i2cData[Data_indent] = Data;
       Data_indent += 1;
     }
@@ -97,19 +122,19 @@ void receiveData(int byteCount) {
       state_i2c = bool(i2cData[0] - 1);
       step_mode = i2cData[5];
       number_of_revolutions = i2cData[1];
-      stepper.setMaxSpeed(i2cData[2]*step_mode*10);
-      stepper.setAcceleration(i2cData[3]*step_mode*10);
-      pause_time = i2cData[4];
+      stepper.setMaxSpeed(i2cData[2] * step_mode * 10);
+      stepper.setAcceleration(i2cData[3] * step_mode * 10);
+      goToSleep = bool(i2cData[4] - 1);
       rotation_done = false;
     }
   }
 }
 
-void motorRotate(){
+void motorRotate() {
   //désactivation du mode sleep
   digitalWrite(SleepModePin, HIGH);
   //définition de la prochaine position
-  stepper.move(400*number_of_revolutions*step_mode);
+  stepper.move(400 * number_of_revolutions * step_mode);
   //déplacement jusqu'à la position précédemment définie, à la vitesse et accélération définie
   stepper.runToPosition();
   //réactivation du mode sleep
@@ -118,6 +143,5 @@ void motorRotate(){
 
 // fonction appelée par requête de la Raspberry
 void sendData() {
-  Serial.println("requête raspi detectee");
   Wire.write(rotation_done);
 }
