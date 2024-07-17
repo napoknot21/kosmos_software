84985f62f19214f1a2f3996ffc41a994290f307a
Tristan <kosmos@konkarlab.bzh>
2024-07-17 11:19:44 +0200

    melody modifiée

--- kosmosV3-env/kosmos_melody.py
+++ kosmosV3-env/kosmos_melody.py
@@ -6,7 +6,7 @@ class KMelody(list, Enum):
 	"""Définition des musique correspondant à chaque état de fonctionnement du KOSMOS"""
 	"""structure des mélodies : [[liste d'enchainement de notes][liste du rythme correspondant (une valeur par note)], vitesse de jeu, rapidité de la note (0 à 1)]"""
 	STARTING_MELODY = [["C7", "D7", "E7", "F7", "G7", "A7", "B7", "C8", "C7", "D7", "E7", "F7", "G7", "A7", "B7", "C8"],[4, 4, 4, 4, 4, 4, 4, 32, 4, 4, 4, 4, 4, 4, 4, 32], 0.01, 0.9]
-	STANDBY_MELODY = [["E7", "E7", "E7", "D7", "E7", "G7"],[8, 8, 16, 8, 8, 8], 0.015, 0.7]
+	STANDBY_MELODY = [["G7", "E7", "E7", "D7", "E7", "G7"],[8, 8, 16, 8, 8, 8], 0.015, 0.7]
 	WORKING_MELODY = [["E7", "G7", "A7", "A7", "A7", "B7", "C8", "C8", "C8", "D8", "B7", "B7", "A7", "G7", "G7", "A7"], [8, 8, 16, 16, 8, 8, 16, 16, 8, 8, 16, 16, 8, 8, 8, 16], 0.018, 0.7]
 	STOPPING_MELODY = [["C8", "G7", "E7", "C7"], [4, 4, 4, 8], 0.02, 0.5]
 	SHUTDOWN_MELODY = [["C8", "B7", "A7", "G7", "F7", "E7", "D7", "C7"], [4, 4, 4, 4, 4, 4, 4, 16], 0.02, 1]
